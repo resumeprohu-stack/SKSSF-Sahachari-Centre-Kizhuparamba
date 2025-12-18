b9933c6e95c9c0592901a675cfa98313a54ff377
ResumePro Hub <resumeprohu@gmail.com>
2025-12-18 14:51:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3229550651-19d69",
+  "appId": "1:584524840978:web:84f54a154e70018794d8cb",
+  "apiKey": "AIzaSyAGddjl4KnPWWwBZ7JFaBSqHgop3_7942k",
+  "authDomain": "studio-3229550651-19d69.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "584524840978"
+};
